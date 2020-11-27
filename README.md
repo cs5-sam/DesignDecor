830b741ea65affdcb821c74f54ca534e6219e3df
Sameer <sameergautam1999@gmail.com>
2020-11-27 17:46:40 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Design Decor
+
+Website using FullStack Technologies for an interior designing
+company. The website will serve the purpose of introducing the company
+to the user and will also display the previous projects done by the
+company. It will also work as the online platform for displaying the
+products of the company.
+
+<a href="https://cs5-sam.github.io/Design-Decor-Main/" target="_blank"><strong>Check Here</strong></a>
---
+++ js/undertaking.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var parent = document.querySelector('.splitview'),
+        topPanel = parent.querySelector('.top'),
+        handle = parent.querySelector('.handle'),
+        skewHack = 0,
+        delta = 0;
+
+    // If the parent has .skewed class, set the skewHack var.
+    if (parent.className.indexOf('skewed') != -1) {
+        skewHack = 1000;
+    }
+
+    parent.addEventListener('mousemove', function(event) {
+        // Get the delta between the mouse position and center point.
+        delta = (event.clientX - window.innerWidth / 2) * 0.5;
+
+        // Move the handle.
+        handle.style.left = event.clientX + delta + 'px';
+
+        // Adjust the top panel width.
+        topPanel.style.width = event.clientX + skewHack + delta + 'px';
+    });
+});
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
